07b293003aabb08f6bf7855e86a70a37cb75dea3
evry1lovesjay <evry1lovesjay@yahoo.com>
2024-11-10 03:45:25 +0100

    fixed issues with _id

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -25,7 +25,7 @@ const MenuPage = async() => {
       {menu.map((category) => (
         <Link
           href={`/menu/${category.slug}`}
-          key={category.id}
+          key={category._id}
           className="w-full h-1/3 bg-cover p-8 md:h-1/2"
           style={{ backgroundImage: `url(${category.img})` }}
         >
